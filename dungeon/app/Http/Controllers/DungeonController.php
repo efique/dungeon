bc1d10fac41aec3d86f6eb212b0dbcc134cd6f72
valentin.barriol <valentin.barriol@ynov.com>
2022-04-14 16:52:23 +0200

    firstpush

--- dungeon/app/Http/Controllers/DungeonController.php
+++ dungeon/app/Http/Controllers/DungeonController.php
@@ -17,4 +17,20 @@ class DungeonController extends Controller
 
         return $data;
     }
+
+    public function genericRequest($url, $token, $method, $body = null)
+    {
+        $client = new Guzzle;
+        if ($method == 'get') {
+            $res = $client->get($url, ['headers' => ['x-auth-token' => $token]]);
+        } else if ($method == 'post') {
+            $res = $client->post($url, ['headers' => ['x-auth-token' => $token], 'body' => $body]);
+        } else if ($method == 'delete') {
+            $res = $client->delete($url, ['headers' => ['x-auth-token' => $token]]);
+        }
+
+        $data['message'] = $res->getBody()->getContents();
+
+        return $data;
+    }
 }
