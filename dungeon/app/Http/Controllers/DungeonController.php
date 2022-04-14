accc564eb7c9025f2b3e60d332879f283cf91d27
valentin.barriol <valentin.barriol@ynov.com>
2022-04-14 17:29:47 +0200

    firstpush

--- dungeon/app/Http/Controllers/DungeonController.php
+++ dungeon/app/Http/Controllers/DungeonController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use GuzzleHttp\Client as Guzzle;
 use Illuminate\Http\Request;
+use GuzzleHttp\Client as Guzzle;
+use GuzzleHttp\Exception\ClientException as GuzzleError;
 
 class DungeonController extends Controller
 {
@@ -15,22 +16,30 @@ class DungeonController extends Controller
         $data['x-auth-token'] = $res->getHeaders()['x-subject-token'];
         $data['message'] = $res->getBody()->getContents();
 
-        return $data;
+        return response()->json($data);
     }
 
     public function genericRequest(Request $request)
     {
         $client = new Guzzle;
-        if ($request['method'] == 'get') {
-            $res = $client->get($request['url'], ['headers' => ['x-auth-token' => $request['token']]]);
-        } else if ($request['method'] == 'post') {
-            $res = $client->post($request['url'], ['headers' => ['x-auth-token' => $request['token']], 'body' => $request['body']]);
-        } else if ($request['method'] == 'delete') {
-            $res = $client->delete($request['url'], ['headers' => ['x-auth-token' => $request['token']]]);
+
+        try {
+            if ($request['method'] == 'get') {
+                $res = $client->get($request['url'], ['headers' => ['x-auth-token' => $request['token']]]);
+            } else if ($request['method'] == 'post') {
+                $res = $client->post($request['url'], ['headers' => ['x-auth-token' => $request['token']], 'body' => $request['body']]);
+            } else if ($request['method'] == 'delete') {
+                $res = $client->delete($request['url'], ['headers' => ['x-auth-token' => $request['token']]]);
+            }
+
+            $data['message'] = $res->getBody()->getContents();
+
+            return response()->json($data);
+        } catch (GuzzleError $e) {
+            $response = $e->getResponse();
+            $responseBodyAsString = $response->getBody()->getContents();
+
+            return response()->json($responseBodyAsString);
         }
-
-        $data['message'] = $res->getBody()->getContents();
-
-        return $data;
     }
 }
