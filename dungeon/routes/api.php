99fa7e723aa9325aa1a8accb124a9cc4a69302ee
valentin.barriol <valentin.barriol@ynov.com>
2022-04-14 16:52:52 +0200

    firstpush

--- dungeon/routes/api.php
+++ dungeon/routes/api.php
@@ -20,3 +20,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('/inscription', [DungeonController::class, 'inscription']);
+
+Route::post('/generic', [DungeonController::class, 'genericRequest']);
